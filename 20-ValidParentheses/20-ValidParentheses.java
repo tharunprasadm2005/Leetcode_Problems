d584228f8a462371d7ac0ef89ad0858016604431
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:09:43 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,18 @@
+// Last updated: 03/09/2025, 10:09:39
+class Solution {
+    public boolean isValid(String s) {
+         Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+
+    return stack.isEmpty();
+    }
+}
