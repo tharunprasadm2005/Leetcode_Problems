b5d8117cb7177e5538159f25adea653e4ac40332
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:07:43 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,24 @@
+// Last updated: 03/09/2025, 10:07:39
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        if(s.length==0){
+            return 0;
+        }
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int i=g.length-1;
+        int j=s.length-1;
+        while(i>=0 && j>=0){
+            if(s[j]>=g[i]){
+                count++;
+                i--;
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+        return count;
+    }
+}
