9d99edbb70655dbee6629ebb2e4ae6cc2719953c
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:08:25 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,23 @@
+// Last updated: 03/09/2025, 10:08:21
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int sum = 0;
+        int[] l = new int[n];
+        int[] r = new int[n];
+        Arrays.fill(l, 1);
+        Arrays.fill(r, 1);
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1])
+                l[i] = l[i - 1] + 1;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1])
+                r[i] = r[i + 1] + 1;
+        }
+        for (int i = 0; i < n; ++i) {
+            sum += Math.max(l[i], r[i]);
+        }
+        return sum;
+    }
+}
