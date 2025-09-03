c67045e06e1f77d1110953f2b94f7a7b298aa2b3
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:08:21 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -0,0 +1,17 @@
+// Last updated: 03/09/2025, 10:08:17
+class Solution {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> ans = new ArrayList<>();
+    postorder(root, ans);
+    return ans;
+  }
+
+  private void postorder(TreeNode root, List<Integer> ans) {
+    if (root == null)
+      return;
+
+    postorder(root.left, ans);
+    postorder(root.right, ans);
+    ans.add(root.val);
+  }
+}
