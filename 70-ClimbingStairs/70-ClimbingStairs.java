efb6665f126c5ab5a34c0fe324dd71cb7f661db8
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:09:05 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,14 @@
+// Last updated: 03/09/2025, 10:09:01
+class Solution {
+  public int climbStairs(int n) {
+    // dp[i] := the number of ways to climb to the i-th stair
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+      dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+  }
+}
