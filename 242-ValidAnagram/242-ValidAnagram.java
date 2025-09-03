2abbc8b5b9dd09b1d4a8f94ce48d8e3c8c3a3c84
Tharun Prasad <151341763+tharunprasadm2005@users.noreply.github.com>
2025-09-03 10:07:57 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,20 @@
+// Last updated: 03/09/2025, 10:07:53
+class Solution {
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length())
+      return false;
+
+    int[] count = new int[26];
+
+    for (final char c : s.toCharArray())
+      ++count[c - 'a'];
+
+    for (final char c : t.toCharArray()) {
+      if (count[c - 'a'] == 0)
+        return false;
+      --count[c - 'a'];
+    }
+
+    return true;
+  }
+}
